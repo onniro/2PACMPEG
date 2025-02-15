0cb3bf215681757398181e1be6c8f29da971e1f0
onniro <onnirokka@gmail.com>
2025-02-15 21:56:53 +0200

    changed executable name to lowercase on linux

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ INCLUDE_DIRS="-I $PWD/deps -I $PWD/deps/imgui -I $PWD/deps/GLFW"
 LINK_DIRS=""
 OBJ_FILES="$PWD/imgui_o_files_linux/*.o"
 LINK_LIBS="-static-libstdc++ -lX11 $PWD/deps/GLFW/linux/libglfw3.a -lGL"
-MISC_FLAGS="-O0 -o 2PACMPEG -gdwarf -finput-charset=UTF-8 -fexec-charset=UTF-8"
+MISC_FLAGS="-O0 -o 2pacmpeg -gdwarf -finput-charset=UTF-8 -fexec-charset=UTF-8"
 DEFINES="-D_2PACMPEG_LINUX=1 -D_2PACMPEG_DEBUG=1 -D_2PACMPEG_RELEASE=0"
 WARNINGLEVEL="-Wno-parentheses -Wno-format -Wno-unicode-whitespace"
 
--- clang_release.sh
+++ clang_release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo '\033[1;33mNOTE: make sure to update version macros in 2pacmpeg.h before releasing 4real!\033[0m\n'
+echo -e '\033[1;33mNOTE: make sure to update version macros in 2pacmpeg.h before releasing 4real!\033[0m\n'
 
 WORKDIR="$PWD/release_linux"
 SOURCES="$PWD/deps/imgui/imgui*.cpp $PWD/linux_2pacmpeg.cpp"
@@ -7,7 +7,7 @@ INCLUDE_DIRS="-I $PWD/deps -I $PWD/deps/imgui -I $PWD/deps/GLFW"
 #LINK_LIBS="-static-libstdc++ -lGL -lglfw"
 STATIC_LIB_DIR1="/usr/lib/x86_64-linux-gnu"
 LINK_LIBS="-lX11 $PWD/deps/GLFW/linux/libglfw3.a -static-libstdc++ -static-libgcc -lGL"
-MISC_FLAGS="-O2 -o 2PACMPEG -s"
+MISC_FLAGS="-O2 -o 2pacmpeg -s"
 DEFINES="-D_2PACMPEG_LINUX=1 -D_2PACMPEG_DEBUG=0 -D_2PACMPEG_RELEASE=1"
 WARNINGLEVEL="-Wno-parentheses -Wno-format -Wno-unicode-whitespace"
 
