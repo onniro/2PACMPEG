79c7d6c46b3646539d2190b6db34dd00b34feee0
onniro <onnirokka@gmail.com>
2025-02-20 18:24:34 +0200

    fix build script

--- clang_release.bat
+++ clang_release.bat
@@ -7,7 +7,7 @@ REM set OBJ_FILES=%CD%\imgui_o_files\*.o
 set INCLUDE_DIRS=-I%CD%\deps -I%CD%\deps\imgui -I%CD%\deps\GLFW
 set LINK_DIRS=-L %CD%\deps\GLFW\win32
 set LINK_LIBS=-static-libstdc++ -static -lunwind -lopengl32 -lgdi32 -lshell32 -lole32 -lglfw3 -lcomctl32 -luuid -lwinmm -lshlwapi
-set MISC_FLAGS=-fno-cxx-exceptions -fno-exceptions -target x86_64-pc-windows-gnu -march=x86-64 -O2 -s -o 2PACMPEG.EXE -Wl,/subsystem:windows,/subsystem:console,/opt:ref
+set MISC_FLAGS=-fno-cxx-exceptions -fno-exceptions -target x86_64-pc-windows-gnu -march=x86-64 -O2 -s -o 2PACMPEG.EXE -Wl,/subsystem:windows,/opt:ref
 
 set DEFINES=-D _2PACMPEG_WIN32=1 -D _2PACMPEG_RELEASE=1 -D _2PACMPEG_DEBUG=0 -D _NO_DEBUG_HEAP=1 -D_2PACMPEG_ENABLE_CHINESE_SIMPLIFIED=0 -D_2PACMPEG_ENABLE_CHINESE_FULL=0
 
