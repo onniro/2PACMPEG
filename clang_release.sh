ba90039ed5501e981f6e300d842bfe301c85fa9e
onniro <onnirokka@gmail.com>
2025-04-10 15:36:37 +0300

    fix build scripts

--- clang_release.sh
+++ clang_release.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-echo "NOTE: make sure to update version macros in 2pacmpeg.h before releasing 4real!"
+#echo "NOTE: make sure to update version macros in 2pacmpeg.h before releasing 4real!"
 
 WORKDIR="$PWD/release_linux"
 SOURCES="$PWD/deps/imgui/imgui*.cpp $PWD/linux_2pacmpeg.cpp"
 INCLUDE_DIRS="-I $PWD/deps -I $PWD/deps/imgui -I $PWD/deps/GLFW"
 #LINK_LIBS="-static-libstdc++ -lGL -lglfw"
 STATIC_LIB_DIR1="/usr/lib/x86_64-linux-gnu"
-LINK_LIBS="-lX11 $PWD/deps/GLFW/linux/libglfw3.a -static-libstdc++ -static-libgcc -lGL"
+LINK_LIBS="-lglfw -static-libstdc++ -static-libgcc -lGL"
+#LINK_LIBS="-lX11 -lglfw -static-libstdc++ -static-libgcc -lGL"
 MISC_FLAGS="-O2 -o 2pacmpeg -s"
 DEFINES="-D_2PACMPEG_LINUX=1 -D_2PACMPEG_DEBUG=0 -D_2PACMPEG_RELEASE=1 -D_2PACMPEG_ENABLE_CHINESE_SIMPLIFIED=0 -D_2PACMPEG_ENABLE_CHINESE_FULL=0"
 WARNINGLEVEL="-Wno-parentheses -Wno-format -Wno-unicode-whitespace"
